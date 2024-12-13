a9924aeae650fbbfc97ba217eb6032445e54ab71
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-12-13 11:07:45 -0500

    Create decrypt.mk

---
+++ decrypt.mk
@@ -0,0 +1,11 @@
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
+
+# Decryption
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
